d02f2bdb02d738af72a09c259f68a610386e4984
Michael Dwan <mpdwan@gmail.com>
2009-12-29 12:01:11 -0600

    Removing bundled Solr in favor of a separate gem

--- lib/flare/tasks.rb
+++ lib/flare/tasks.rb
@@ -1,51 +1,6 @@
 require 'escape'
 
 namespace :flare do
-  namespace :solr do
-    desc 'Start the Solr instance'
-    task :start => :environment do
-      solr_home = Flare::Configuration.server.solr_home
-      data_dir = Flare::Configuration.server.data_dir
-      pid_dir = Flare::Configuration.server.pid_dir
-      port = Flare::Configuration.server.port 
-      log_file = File.join(Flare::Configuration.server.log_dir, 'solr.log')
-      log_level = Flare::Configuration.server.log_level
-      script_file = File.dirname(__FILE__) + '/../../bin/flare-solr'
-
-      [data_dir, pid_dir].each { |path| FileUtils.mkdir_p(path) }
-      
-      command = [script_file, 'start', '-p', port.to_s, '-d', data_dir, '--pid-dir', pid_dir, '-l', log_level, '--log-file', log_file]
-      if solr_home
-        command << '-s' << solr_home
-      end
-      system(Escape.shell_command(command))
-    end
-
-    desc 'Run the Solr instance in the foreground'
-    task :run => :environment do
-      data_path = Flare::Configuration.server.data_dir
-      solr_home = Flare::Configuration.server.solr_home
-      port = Flare::Configuration.server.port
-      script_file = File.dirname(__FILE__) + '/../../bin/flare-solr'
-
-      FileUtils.mkdir_p(data_path)
-      command = [script_file, 'run', '-p', port.to_s, '-d', data_path]
-      if solr_home
-        command << '-s' << solr_home
-      end
-      exec(Escape.shell_command(command))
-    end
-
-    desc 'Stop the Solr instance'
-    task :stop => :environment do
-      script_file = File.dirname(__FILE__) + '/../../bin/flare-solr'
-
-      FileUtils.cd(Flare::Configuration.server.pid_dir) do
-        system(Escape.shell_command([script_file, 'stop']))
-      end
-    end
-  end
-    
   desc 'Rebuild the Solr index for all searchable models'
   task :rebuild_index => :environment do      
     Flare.indexed_models.each do |model|
